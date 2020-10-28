5a2db663b40d6b5ecd4569f9ea3fd69552e639df
andre4238 <70895006+andre4238@users.noreply.github.com>
2020-10-28 10:20:30 +0100

    Add files via upload

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            generateRandomPassword();
+        }
+
+        public static void generateRandomPassword()
+        {
+            Random random = new Random();
+            int length = 16;
+            var rString = "";
+            for (var i = 0; i < length; i++)
+            {
+                rString += ((char)(random.Next(1, 26) + 64)).ToString().ToLower();
+            }
+            Console.WriteLine(rString);
+        }
+
+    }
+}
